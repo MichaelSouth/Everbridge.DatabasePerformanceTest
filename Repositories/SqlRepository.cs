9c8c4dafb41c82881c60b2d459dbb141ebed2036
michael south <Michaels@cnluk.com>
2021-11-07 15:19:07 +0000

    Add SQL repository

--- Repositories/SqlRepository.cs
+++ Repositories/SqlRepository.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace Everbridge.DatabasePerformanceTest.Repositories
 {
     public class SqlRepository : IDatabasePerformanceRepository
     {
+        private const string ConnectionString = @"Data Source=(local);Initial Catalog=Test;User ID=sa;Password=password1.;MultipleActiveResultSets=True";
+
         void IDatabasePerformanceRepository.ExecuteTask(DatabasePerformanceTask task)
         {
-            throw new NotImplementedException();
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+
+                for (var i = 0; i < task.IterationCount; i++)
+                {
+                    var sqlCommand = new SqlCommand("INSERT INTO [dbo].[PerfTest] ([Data]) VALUES(@data)", sqlConnection) { CommandType = CommandType.Text };
+                    sqlCommand.Parameters.AddWithValue("@data", task.Data);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
